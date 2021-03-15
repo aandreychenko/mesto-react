244d635222c2c7ccca523fe69ffd2dfe8f22e964
Alexander Andreychenko <the-ravenclo@yandex.ru>
2021-03-15 13:11:03 +0300

    porting Mesto App to Reaact

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+export default function Card(props) {
+  const handleImageClick = () => {
+    props.handleCardImage(props);
+  }
+
+  return (
+    <article className="element" key={props.card._id}>
+      <img className="element__image" src={props.card.link} alt="Фотография места" onClick={handleImageClick} />
+      <div className="element__info">
+        <h2 className="element__place-name">{props.card.name}</h2>
+        <div className="element__like-block">
+          <button type="button" className="element__like-button"></button>
+          <p className="element__like-counter">0</p>
+        </div>
+      </div>
+      <button type="button" className="element__trash-button"></button>
+    </article>
+  )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,57 @@
-export default function Header() {
+import React from "react";
+import api from "../utils/api"
+import Card from "./Card"
+
+export default function Main(props) {
+
+  let [{name: userName, about: userDescription, avatar: userAvatar}, setProfile] = React.useState({name:'', about:'', avatar: ''});
+
+  React.useEffect(() => {
+    api.getUserInfo()
+        .then((res) => {
+          setProfile(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+  }, [])
+
+  let [cards, setCards] = React.useState([]);
+
+  React.useEffect(() => {
+    api.getCards()
+        .then((res) => {
+          setCards(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+  }, [])
+
+  const mainCardImage = (data) => {
+    props.onOpenImage && props.onOpenImage(data);
+  }
+
   return (
       <main className="main">
         <section className="profile">
-          <a className="profile__avatar-container">
-            <img className="profile__avatar" alt="Фотография профиля" />
+          <a className="profile__avatar-container" onClick={props.onEditAvatar}>
+            <img className="profile__avatar" alt="Фотография профиля" src={userAvatar} />
           </a>
           <div className="profile__info">
             <div className="profile__info-heading">
-              <h1 className="profile__name"></h1>
-              <button type="button" className="profile__edit-button" aria-label="Редактировать профиль"></button>
+              <h1 className="profile__name">{userName}</h1>
+              <button type="button" className="profile__edit-button" aria-label="Редактировать профиль" onClick={props.onEditProfile}></button>
             </div>
-            <p className="profile__caption"></p>
+            <p className="profile__caption">{userDescription}</p>
           </div>
-          <button type="button" className="profile__add-button" aria-label="Добавить карточку"></button>
+          <button type="button" className="profile__add-button" aria-label="Добавить карточку" onClick={props.onAddPlace}></button>
         </section>
         <section className="elements">
+          {cards.map((item) => (
+            <Card card={item} key={item._id} handleCardImage={mainCardImage} />
+          ))}
         </section>
       </main>
-  );
+  )
 }
