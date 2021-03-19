eb5e05382ce3c46b9ded980676faabd08b623199
Alexander Andreychenko <the-ravenclo@yandex.ru>
2021-03-20 00:08:42 +0300

    fix warning on server starting

--- src/components/Main.js
+++ src/components/Main.js
@@ -35,17 +35,17 @@ export default function Main(props) {
   return (
       <main className="main">
         <section className="profile">
-          <a className="profile__avatar-container" onClick={props.onEditAvatar}>
+          <button className="profile__avatar-container" onClick={props.onEditAvatar}>
             <img className="profile__avatar" alt="Фотография профиля" src={userAvatar} />
-          </a>
+          </button>
           <div className="profile__info">
             <div className="profile__info-heading">
               <h1 className="profile__name">{userName}</h1>
-              <button type="button" className="profile__edit-button" aria-label="Редактировать профиль" onClick={props.onEditProfile}></button>
+              <button type="button" className="profile__edit-button" aria-label="Редактировать профиль" onClick={props.onEditProfile} />
             </div>
             <p className="profile__caption">{userDescription}</p>
           </div>
-          <button type="button" className="profile__add-button" aria-label="Добавить карточку" onClick={props.onAddPlace}></button>
+          <button type="button" className="profile__add-button" aria-label="Добавить карточку" onClick={props.onAddPlace} />
         </section>
         <section className="elements">
           {cards.map((item) => (
